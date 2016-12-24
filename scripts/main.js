b005705faec9fa3d11e89f3deca8b6b21415b16a
Will D <wpdonabedian@gmail.com>
2016-12-23 21:33:48 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -19,14 +19,14 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
   var myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
-  myHeading.textContent = 'Mozilla is cool, ' + myName;
+  myHeading.textContent = 'Welcome, ' + myName + '!';
 }
 
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   var storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  myHeading.textContent = 'Welcome, ' + storedName + '!';
 }
 
 myButton.onclick = function() {
